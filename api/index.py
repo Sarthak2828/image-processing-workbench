5b3eaf68833183e14b5c223bb1b64c5f02c2bd07
Sarthak Gaba <sarthakgaba28@gmail.com>
2026-07-13 10:44:55 +0530

    Make uploaded images scale up to take full panel space

--- api/index.py
+++ api/index.py
@@ -524,8 +524,8 @@ HTML_CONTENT = """<!DOCTYPE html>
         }
 
         .canvas-container img {
-            max-width: 100%;
-            max-height: 100%;
+            width: 100%;
+            height: 100%;
             object-fit: contain;
         }
 
